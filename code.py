11d9b541966dcb30006b05c666eee6b32cdf792f
MaksimObozniy <125642497+MaksimObozniy@users.noreply.github.com>
2024-07-22 20:04:26 +0700

    Update and rename code to code.py

--- code.py
+++ code.py
@@ -23,25 +23,35 @@ def has_lower_letters(password):
 
 def has_symbols(password):
     return any(not letter.isalnum() for letter in password)
-            
+
+
 def resultat(password):
     checks = [
-        is_very_long, has_str, 
-        has_digit, has_upper_letters, 
-        has_lower_letters, has_symbols
-    ]
+        is_very_long,
+        has_str, 
+        has_digit,
+        has_upper_letters, 
+        has_lower_letters,
+        has_symbols
+        ]
     return sum(2 for check in checks if check(password))
 
 
-def on_ask_change(score, new_edit_text):
+def on_ask_change(new_edit_text):
     score = resultat(new_edit_text)
     reply.set_text(f"Рейтинг вашего пароля: {score}")
 
 
-ask = urwid.Edit('Введите пароль: ', mask='*')
-reply = urwid.Text("Рейтинг вашего пароля: 0")
-menu = urwid.Pile([ask, reply])
-menu = urwid.Filler(menu, valign='top')
+def main():
+    global reply
+    ask = urwid.Edit('Введите пароль: ', mask='*')
+    reply = urwid.Text("Рейтинг вашего пароля: 0")
+    menu = urwid.Pile([ask, reply])
+    menu = urwid.Filler(menu, valign='top')
+    
+    urwid.connect_signal(ask, 'change', on_ask_change)
+    urwid.MainLoop(menu).run()
 
-urwid.connect_signal(ask, 'change', on_ask_change)
-urwid.MainLoop(menu).run()
+
+if __name__ == "__main__":
+    main()
